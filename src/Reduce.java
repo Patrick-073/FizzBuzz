ab469f87a5fcd99c6d5e6d1d5b4e107b4ab24869
Ethan <ethannnn73@gmail.com>
2023-09-12 11:15:11 -0400

    Update

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,8 +1,4 @@
 public class Reduce {
-//    Starting with a number n, if n is even divide it by 2. If n is odd, subtract 1. Repeat.
-//    For example, if you start with n = 2, the answer is 2. Details: 2 (even, divide by 2) --> 1 (odd, subtract 1) --> 0.
-//    In Reduce.java, write a main method that prints how many steps it takes to reach 0 if you start at 100. Add this file to your project, commit,
-//    and push.
     public static void main(String[] args) {
     int i = 100;
     int t = 0;
